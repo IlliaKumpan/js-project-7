77e928fe664b2368c3e6ad5d7e48ebd8330028de
IlliaKumpan <ilyakumpan11@gmail.com>
2026-03-06 16:48:00 +0000

    Deploying to gh-pages from @ IlliaKumpan/js-project-7@7ce3a08e9461d3b502baa6493a2c26eb81ca13ae 🚀

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-import{A as p,S as f,N as m,P as g}from"./assets/vendor-D4q9fNXw.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function o(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerPolicy&&(s.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?s.credentials="include":t.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function a(t){if(t.ep)return;t.ep=!0;const s=o(t);fetch(t.href,s)}})();new p(".accordion-container",{elementClass:"ac",triggerClass:"ac-trigger",panelClass:"ac-panel",openMultiple:!1,collapse:!0,duration:300,showIcon:!0});const h="https://paw-hut.b.goit.study",y="/api/feedbacks",v=document.querySelector(".feedbacks"),b=document.querySelectorAll(".stories-btn"),d=document.querySelector(".loader");function S(){d.classList.remove("loader-hidden")}function L(){d.classList.add("loader-hidden")}const c=new f(".stories-swiper",{modules:[m,g],initialSlide:0,slidesPerGroup:1,slidesPerView:1,spaceBetween:32,navigation:{nextEl:"#stories .btn-next",prevEl:"#stories .btn-back"},pagination:{el:"#stories .stories-navigation .swiper-pagination",clickable:!0,dynamicBullets:!0,dynamicMainBullets:5},breakpoints:{768:{slidesPerGroup:2,slidesPerView:2},1440:{slidesPerGroup:2,slidesPerView:2}}});S();fetch(`${h}${y}`).then(e=>e.json()).then(e=>{w(e.feedbacks),c.update(),c.pagination.render(),c.pagination.update()}).catch(e=>console.error("Error fetching data:",e)).finally(()=>{L()});function k(e){const r=Math.round(e*2)/2,o=Math.floor(r),a=r%1!==0?1:0,t=5-o-a,s=`
+import{A as f,S as m,N as g,P as y}from"./assets/vendor-D4q9fNXw.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const i of s.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&a(i)}).observe(document,{childList:!0,subtree:!0});function r(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerPolicy&&(s.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?s.credentials="include":t.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function a(t){if(t.ep)return;t.ep=!0;const s=r(t);fetch(t.href,s)}})();new f(".accordion-container",{elementClass:"ac",triggerClass:"ac-trigger",panelClass:"ac-panel",openMultiple:!1,collapse:!0,duration:300,showIcon:!0});const n=document.querySelector("[data-menu]"),h=document.querySelector("[data-menu-open]"),v=document.querySelector("[data-menu-close]"),L=document.querySelectorAll(".mobile-nav a");h.addEventListener("click",()=>{n.classList.add("is-open"),document.body.style.overflow="hidden"});v.addEventListener("click",()=>{n.classList.remove("is-open"),document.body.style.overflow=""});L.forEach(e=>{e.addEventListener("click",()=>{n.classList.remove("is-open")})});document.addEventListener("keydown",e=>{e.key==="Escape"&&n.classList.contains("is-open")&&n.classList.remove("is-open")});n.addEventListener("click",e=>{e.target===n&&n.classList.remove("is-open")});const b="https://paw-hut.b.goit.study",S="/api/feedbacks",k=document.querySelector(".feedbacks"),w=document.querySelectorAll(".stories-btn"),u=document.querySelector(".loader");function E(){u.classList.remove("loader-hidden")}function q(){u.classList.add("loader-hidden")}const l=new m(".stories-swiper",{modules:[g,y],initialSlide:0,slidesPerGroup:1,slidesPerView:1,spaceBetween:32,navigation:{nextEl:"#stories .btn-next",prevEl:"#stories .btn-back"},pagination:{el:"#stories .stories-navigation .swiper-pagination",clickable:!0,dynamicBullets:!0,dynamicMainBullets:5},breakpoints:{768:{slidesPerGroup:2,slidesPerView:2},1440:{slidesPerGroup:2,slidesPerView:2}}});E();fetch(`${b}${S}`).then(e=>e.json()).then(e=>{A(e.feedbacks),l.update(),l.pagination.render(),l.pagination.update()}).catch(e=>console.error("Error fetching data:",e)).finally(()=>{q()});function P(e){const o=Math.round(e*2)/2,r=Math.floor(o),a=o%1!==0?1:0,t=5-r-a,s=`
     <svg class="star star-filled">
       <use href="../img/icons.svg#star-filled"></use>
     </svg>
-  `.repeat(o),n=a?`
+  `.repeat(r),i=a?`
       <span class="star-half" aria-hidden="true">
         <svg class="star star-outline">
           <use href="../img/icons.svg#star-outline"></use>
@@ -13,19 +13,19 @@ import{A as p,S as f,N as m,P as g}from"./assets/vendor-D4q9fNXw.js";(function()
           </svg>
         </span>
       </span>
-    `:"",u=`
+    `:"",p=`
     <svg class="star star-outline">
       <use href="../img/icons.svg#star-outline"></use>
     </svg>
-  `.repeat(t);return s+n+u}function w(e){const r=e.map(o=>`
+  `.repeat(t);return s+i+p}function A(e){const o=e.map(r=>`
     <div class="swiper-slide">
       <div class="feedback-card">
         <div class="rating">
-          <div class="stars">${k(o.rate)}</div>
+          <div class="stars">${P(r.rate)}</div>
         </div>
-        <p class="feedback-description">${o.description}</p>
-        <p class="feedback-author">${o.author}</p>
+        <p class="feedback-description">${r.description}</p>
+        <p class="feedback-author">${r.author}</p>
       </div>
     </div>
-  `).join("");v.innerHTML=r}b.forEach(e=>{e.addEventListener("click",()=>e.blur())});const i=document.querySelector(".modal-pet-backdrop");document.querySelector(".modal-pet-image");document.querySelector(".modal-pet-species");document.querySelector(".modal-pet-name");document.querySelector(".modal-pet-age");document.querySelector(".modal-pet-gender");document.querySelector(".modal-pet-desc");document.querySelector(".modal-pet-health");document.querySelector(".modal-pet-behavior");i.addEventListener("click",P);document.addEventListener("keydown",q);function l(){i.classList.add("is-hidden"),document.body.style.overflow=""}function P(e){if(!i.classList.contains("is-hidden")){if(e.target===i||e.target.closest(".modal-pet-close-btn")){l();return}e.target.closest(".modal-pet-adopt-btn")&&E()}}function q(e){i.classList.contains("is-hidden")||e.key==="Escape"&&l()}function E(){l()}
+  `).join("");k.innerHTML=o}w.forEach(e=>{e.addEventListener("click",()=>e.blur())});const c=document.querySelector(".modal-pet-backdrop");document.querySelector(".modal-pet-image");document.querySelector(".modal-pet-species");document.querySelector(".modal-pet-name");document.querySelector(".modal-pet-age");document.querySelector(".modal-pet-gender");document.querySelector(".modal-pet-desc");document.querySelector(".modal-pet-health");document.querySelector(".modal-pet-behavior");c.addEventListener("click",B);document.addEventListener("keydown",M);function d(){c.classList.add("is-hidden"),document.body.style.overflow=""}function B(e){if(!c.classList.contains("is-hidden")){if(e.target===c||e.target.closest(".modal-pet-close-btn")){d();return}e.target.closest(".modal-pet-adopt-btn")&&O()}}function M(e){c.classList.contains("is-hidden")||e.key==="Escape"&&d()}function O(){d()}
 //# sourceMappingURL=index.js.map
